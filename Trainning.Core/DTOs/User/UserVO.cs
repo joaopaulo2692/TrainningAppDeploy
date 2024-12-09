b85c40a86379e3e1ab91094e2f819d49dadc8cfc
joaop <joaopaulo2692@gmail.com>
2024-12-09 15:13:13 -0300

    core

---
+++ Trainning.Core/DTOs/User/UserVO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainningApp.Core.DTO.User
+{
+    public class UserVO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string Observation { get; set; }
+        public string Phone { get; set; }
+        public string Cpf { get; set; }
+        public string Goal { get; set; }
+
+    
+    }
+}
