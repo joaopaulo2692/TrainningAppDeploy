1a2979907029f9ca018913d98e5049137fb8e8f4
joaop <joaopaulo2692@gmail.com>
2024-12-09 15:11:54 -0300

    up

---
+++ Trainning.Core/Entities/Muscle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainningApp.Core.Entities
+{
+    public class Muscle
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<Exercise> Exercises { get; set; }
+    }
+}
---
+++ Trainning.Core/Entities/TrainningExercise.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainningApp.Core.Entities
+{
+    public class TrainningExercise : BaseEntities
+    {
+        public int Id { get; set; }
+        public int Set { get; set; }
+        public string Reps { get; set; }
+        public string Interval { get; set; }
+        public string Weight { get; set; }
+        public int Ordenation { get; set; }
+        public string Info { get; set; }
+        public int ExerciseId { get; set; }
+        public int TrainningDayId { get; set; }
+
+        public virtual Exercise Exercise { get; set; }
+        public virtual TrainningDay TrainningDay { get; set; }
+
+    }
+}
