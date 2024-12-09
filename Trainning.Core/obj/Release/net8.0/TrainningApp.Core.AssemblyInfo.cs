0d63cb07ab6fc4e681fc6b30ce88b87b0cee0094
joaop <joaopaulo2692@gmail.com>
2024-12-09 16:06:21 -0300

    teste

--- Trainning.Core/obj/Release/net8.0/TrainningApp.Core.AssemblyInfo.cs
+++ Trainning.Core/obj/Release/net8.0/TrainningApp.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrainningApp.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1a983f80a6ad764341f4c9008790a151ac4b99a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7de47e0302eadef807318605d5f18a6a3f4201e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrainningApp.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrainningApp.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
